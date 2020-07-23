6eeb8064e77657460ced5f643b5421df981f1e12
Jonas Elan <jonas.elan@gmail.com>
2020-07-22 22:30:26 -0300

    setup react navigation 4

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import {StatusBar} from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      {/* <Routes /> */}
+      <Routes />
     </>
   );
 };
