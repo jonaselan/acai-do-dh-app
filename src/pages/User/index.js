bc788a433f5eff3be06d0415255a9add88a58b62
Jonas Elan <jonas.elan@gmail.com>
2020-07-23 20:51:34 -0300

    Use navigation with tabs

---
+++ src/pages/User/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+// import {ActivityIndicator} from 'react-native';
+// import api from '../../services/api';
+
+import {Container, Text} from './styles';
+
+export default class User extends Component {
+  static navigationOptions = {
+    title: 'User',
+  };
+
+  render() {
+    return <Container />;
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import Main from './pages/Main';
 import User from './pages/User';
 
 const Routes = createAppContainer(
-  createStackNavigator(
+  createBottomTabNavigator(
     {
       Main,
       User,
