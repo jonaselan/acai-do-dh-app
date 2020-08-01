310efbeb62d78a7dcebc8b3051016102ec32a647
Jonas Elan <jonas.elan@gmail.com>
2020-08-01 09:39:01 -0300

    Create form to add a sale

--- src/pages/Sale/New/index.js
+++ src/pages/Sale/New/index.js
@@ -1,17 +1,71 @@
 import React, {Component} from 'react';
-// import {Keyboard, ActivityIndicator} from 'react-native';
-// import AsyncStore from '@react-native-community/async-storage';
+import {ActivityIndicator} from 'react-native';
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+  RadioButtonLabel,
+} from 'react-native-simple-radio-button';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 // import api from '../../services/api';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container} from './styles';
+import {Container, Form, Input, SubmitButton, SubmitButtonText} from './styles';
 
 export default class NewSale extends Component {
   static navigationOptions = {
     title: 'Cadastar venda',
   };
 
+  state = {
+    loading: false,
+    payment_method: 'cash',
+  };
+
   render() {
-    return <Container />;
+    const {loading, payment_method} = this.state;
+    const radio_props = [
+      {label: 'Cartão de Crédito', value: 'credit_card'},
+      {label: 'Dinheiro', value: 'cash'},
+    ];
+
+    return (
+      <Container>
+        <Form>
+          <RadioForm
+            radio_props={radio_props}
+            initial={payment_method}
+            formHorizontal={true}
+            buttonColor={'#7159c1'}
+            animation={true}
+            onPress={(value) => {
+              this.setState({payment_method: value});
+            }}
+          />
+
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Valor"
+            returnKeyType="next"
+          />
+
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Troco"
+            returnKeyType="send"
+          />
+        </Form>
+
+        <SubmitButton loading={loading} onPress={this.handleAddUser}>
+          {loading ? (
+            <ActivityIndicator color="#FFF" />
+          ) : (
+            <Icon name="add" size={20} color="#FFF">
+              <SubmitButtonText>Criar</SubmitButtonText>
+            </Icon>
+          )}
+        </SubmitButton>
+      </Container>
+    );
   }
 }
--- src/pages/Sale/New/styles.js
+++ src/pages/Sale/New/styles.js
@@ -1,22 +1,23 @@
-import styled from 'styled-components'
-// NOTE: usar essa lib para gerar botões adaptaveis para ambas as plataformas
-import { RectButton } from 'react-native-gesture-handler'
+import styled from 'styled-components';
+import {RectButton} from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
+  padding: 0 30px;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Form = styled.View`
-  flex-direction: row;
-  padding-bottom: 20px;
+  align-self: stretch;
+  margin-top: 50px;
   border-bottom-width: 1px;
 `;
 
 export const Input = styled.TextInput.attrs({
   placeholderTextColor: '#999',
 })`
-  flex: 1;
+  margin-bottom: 10px;
   height: 40px;
   background: #eee;
   border-radius: 4px;
@@ -24,56 +25,7 @@ export const Input = styled.TextInput.attrs({
   border: 1px solid #eee;
 `;
 
-// NOTE: vantagem do style-componet: estilizar components n nativos
 export const SubmitButton = styled(RectButton)`
-  justify-content: center;
-  align-items: center;
-  background: #7159c1;
-  border-radius: 4px;
-  margin-left: 10px;
-  padding: 0 12px;
-  opacity: ${props => (props.loading ? 0.7 : 1)}
-`;
-
-// NOTE: essa propriedade é para esconder a barra de rolagem
-export const List = styled.FlatList.attrs({
-  showsVerticalScrollIndicator: false,
-})`
-  margin-top: 20px;
-`;
-
-export const User = styled.View`
-  align-items: center;
-  margin: 0 20px 30px;
-`;
-
-// NOTE: background para criar uma especie de placeholder enquanto a imagem não carrega
-export const Avatar = styled.Image`
-  width: 64px;
-  height: 64px;
-  border-radius: 32px;
-  background: #eee;
-`;
-
-export const Name = styled.Text`
-  font-size: 14px;
-  color: #333;
-  font-weight: bold;
-  margin-top: 5px;
-  text-align: center;
-`;
-
-export const Bio = styled.Text.attrs({
-  numberOfLines: 2
-})`
-  font-size: 13px;
-  line-height: 18px;
-  color: #999;
-  margin: 5px;
-  text-align: center;
-`;
-
-export const ProfileButton = styled(RectButton)`
   margin-top: 10px;
   align-self: stretch;
   border-radius: 4px;
@@ -81,9 +33,10 @@ export const ProfileButton = styled(RectButton)`
   justify-content: center;
   align-items: center;
   height: 36px;
+  opacity: ${(props) => (props.loading ? 0.7 : 1)};
 `;
 
-export const ProfileButtonText = styled.Text`
+export const SubmitButtonText = styled.Text`
   font-size: 14px;
   font-weight: bold;
   color: #fff;
