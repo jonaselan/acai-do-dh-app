7d6607e59fadb4d9204c15d49dc6c3fe2589f1a2
Jonas Elan <jonas.elan@gmail.com>
2020-08-15 11:07:34 -0300

    List sales

--- src/pages/Sale/index.js
+++ src/pages/Sale/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
-// import {Keyboard, ActivityIndicator} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ActivityIndicator} from 'react-native';
 // import AsyncStore from '@react-native-community/async-storage';
-// import api from '../../services/api';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Button, ButtonText} from './styles';
+import {
+  Container,
+  Button,
+  ButtonText,
+  SaleList,
+  Card,
+  Left,
+  Right,
+  CreatedAt,
+  Value,
+  PaymentMethod,
+  DeliveryMethod,
+} from './styles';
 
 export default function Sale({navigation}) {
-  function handleNavigate() {
-    // const {navigation} = this.props;
+  const [loading, setLoading] = useState(false);
+  const [sales, setSales] = useState([]);
 
+  function handleNavigate() {
     navigation.navigate('NewSale');
+  }
+
+  useEffect(() => {
+    loadSales();
+  }, []);
+
+  async function loadSales() {
+    setLoading(true);
+    const response = await api.get('sales');
+
+    setSales(response.data);
+    setLoading(false);
   }
 
   return (
@@ -18,6 +43,48 @@ export default function Sale({navigation}) {
       <Button onPress={() => handleNavigate()}>
         <ButtonText> Adicionar venda </ButtonText>
       </Button>
+      {loading ? (
+        <ActivityIndicator color="#000" />
+      ) : (
+        <SaleList
+          data={sales}
+          keyExtractor={(sale) => sale.id}
+          renderItem={({item}) => (
+            <Card>
+              <Left>
+                <Value>R$ {item.value}</Value>
+                <CreatedAt>20/07/2000</CreatedAt>
+              </Left>
+
+              <Right>
+                {item.delivery_method == 'delivery' ? (
+                  <DeliveryMethod>
+                    <Icon name="home" size={15} color="#333" />
+                    Entrega em casa
+                  </DeliveryMethod>
+                ) : (
+                  <DeliveryMethod>
+                    <Icon name="shop" size={15} color="#333" />
+                    Retirada no local
+                  </DeliveryMethod>
+                )}
+
+                {item.payment_method == 'cash' ? (
+                  <PaymentMethod>
+                    <Icon name="attach-money" size={15} color="#333" />
+                    Dinheiro
+                  </PaymentMethod>
+                ) : (
+                  <PaymentMethod>
+                    <Icon name="credit-card" size={15} color="#333" />
+                    Cartão de Crédito
+                  </PaymentMethod>
+                )}
+              </Right>
+            </Card>
+          )}
+        />
+      )}
     </Container>
   );
 }
--- src/pages/Sale/styles.js
+++ src/pages/Sale/styles.js
@@ -7,6 +7,12 @@ export const Container = styled.View`
   padding: 30px;
 `;
 
+export const SaleList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
 export const Button = styled(RectButton)`
   margin-top: 10px;
   align-self: stretch;
@@ -22,4 +28,53 @@ export const ButtonText = styled.Text`
   font-weight: bold;
   color: #fff;
   text-transform: uppercase;
+`;
+
+export const Card = styled.View`
+  flex-direction: row;
+  padding: 20px;
+  border-bottom-width: 1px;
+  border: #7159c1 1px solid;
+  border-radius: 4px;
+  margin-bottom: 5px;
+`;
+
+export const Left = styled.View`
+  flex: 1;
+  height: 40px;
+  /* border-radius: 4px; */
+  /* background: #eee; */
+  /* border: 1px solid #eee; */
+`;
+
+export const Value = styled.Text`
+  font-weight: bold;
+  font-size: 30px;
+  color: #333;
+`;
+
+export const Right = styled.View`
+  justify-content: center;
+  align-items: flex-start;
+  border-radius: 4px;
+  padding: 0 52px;
+  /* background: #831; */
+`;
+
+export const CreatedAt = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+  color: #333;
+`;
+
+export const PaymentMethod = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+  color: #333;
+`;
+
+export const DeliveryMethod = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+  color: #333;
 `;
